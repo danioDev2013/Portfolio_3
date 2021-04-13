bd5addf7f47c5b10f204101ec4babd9b5d32b303
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-13 10:18:36 -0700

    this is my initial commit of the basic html

--- controllers/api/user-routes.js
+++
@@ -1,6 +0,0 @@
-//Imports
-const router = require("express").Router();
-const { User } = require("../../models");
-const auth = require("../../utils/auth");
-
-//Create info path
--- db/schema.sql
+++
@@ -1,5 +0,0 @@
--- Clear and remake the database to start from zero --
-DROP DATABASE IF EXISTS portfolio_db;
-
--- Make the database --
-CREATE DATABASE portfolio_db;
