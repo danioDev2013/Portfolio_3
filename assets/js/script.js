4ea5dc7a9019566fe04f2b1f42f7fa1e110bb8af
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-04-13 11:12:08 -0700

    fixed a few things, commit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    window.scrollTo(0,0);
+  
+  //selectors
+  var bttn = $(".show");
+  var navBrand = $(".navbar-brand");
+  
+  
+  //creates brand
+  navBrand.text("<DO>");
+  bttn.text("Resume");
+  
+  
+  var content = 'Hi, my name is Danielle!';
+  
+  var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+  
+  
+  $(ele).hide().appendTo('h5').each(function (i) {
+      $(this).delay(100 * i).css({
+          display: 'inline',
+          opacity: 0
+      }).animate({
+          opacity: 1
+      }, 100);
+  });
+  $(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+  
+  
+  
+  
+  
+  //function calls
+  typeWriter();
+  featuredProjects();
+  
+  
+  
+    //button to more
+    var homeBtn = $('<button class="more" id="homeBtn">').text("Home");
+    $(".homeBTN").append(homeBtn);
+  
+    //home button click function
+    $("#homeBtn").on("click", function(){
+      window.location.reload();
+      window.scrollTo(0,0);
+    })
+  
+     
+  });
+  
